aca908816d942f3d41ea7227e76e5f15077a46d2
saeedkhan83 <122628958+saeedkhan83@users.noreply.github.com>
2024-10-10 12:33:11 +0500

    first commit

---
+++ lib/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
