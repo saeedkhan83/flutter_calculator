ffa67ef882965ca66063f02594b4750e051963e3
saeedkhan83 <122628958+saeedkhan83@users.noreply.github.com>
2024-10-22 12:58:53 +0500

    Added Calculator Button on Screen

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/widgets/calculator_button.dart';
 
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({super.key});
@@ -10,6 +11,47 @@ class CalculatorApp extends StatefulWidget {
 class _CalculatorAppState extends State<CalculatorApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(),
+          Container(),
+          const SizedBox(
+            height: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CalculatorButton(
+                text: 'AC',
+                color: Colors.white,
+                textcolor: const Color.fromARGB(255, 241, 85, 74),
+                textsize: 50,
+                callback: () {},
+              ),
+              CalculatorButton(
+                text: 'C',
+                color: Colors.white,
+                textcolor: const Color.fromARGB(255, 241, 85, 74),
+                textsize: 50,
+                callback: () {},
+              ),
+              CalculatorButton(
+                text: 'M',
+                color: Colors.white,
+                textcolor: const Color.fromARGB(255, 241, 85, 74),
+                textsize: 50,
+                callback: () {},
+              ),
+            ],
+          ),
+          const Row(),
+          const Row(),
+          const Row(),
+          const Row(),
+        ],
+      ),
+    );
   }
 }
